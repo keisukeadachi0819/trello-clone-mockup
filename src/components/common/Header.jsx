f233bf2df5734659f6003b3f9fa527a105348f65
keisukeadachi0819 <92622310+keisukeadachi0819@users.noreply.github.com>
2022-06-23 23:21:34 +0900

    ヘッダーロゴの追加とレイアウト整形

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,7 +4,9 @@ export const Header = () => {
   return (
     <header className='app-header'>
       <nav className='app-header-nav'>
-        <a className='app-header-logo' href="/">Trello Clone</a>
+        <a className='app-header-logo' href="/">
+          <img src={`${process.env.PUBLIC_URL}/img/header/logo.gif`} alt="" />
+        </a>
       </nav>
     </header>
   )
